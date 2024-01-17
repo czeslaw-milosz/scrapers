a5cbf35cf5c5100d6a64a18e7776634de695c152
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2024-01-17 02:22:16 +0100

    Ugly fix for dockerized deployment
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- scrapers/olx/olx/settings.py
+++ scrapers/olx/olx/settings.py
@@ -91,8 +91,8 @@ USER_AGENT = latest_user_agents.get_random_user_agent()
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "scrapers.common.pipelines.BatchDeltaExportPipeline": 300,
-   "scrapers.common.pipelines.RootImagesPipeline": 1,
+   "olx.pipelines.BatchDeltaExportPipeline": 300,
+   "olx.pipelines.RootImagesPipeline": 1,
    # "scrapy.pipelines.images.ImagesPipeline": 1,
 
 }
@@ -131,7 +131,7 @@ FEED_EXPORT_ENCODING = "utf-8"
 #         "overwrite": True,
 #     }
 # }
-IMAGES_STORE = f"s3://test-lake/images/{BOT_NAME}/"
+IMAGES_STORE = f"s3://housingdatalake/images/{BOT_NAME}/"
 AWS_ENDPOINT_URL = "http://minio:9000"
 AWS_ACCESS_KEY_ID = "admin"
 AWS_SECRET_ACCESS_KEY = "adminadmin"
@@ -141,7 +141,7 @@ PULSAR_URL = "pulsar://localhost:6650"
 PULSAR_TOPIC = "crawl"
 
 EXPORT_BATCH_SIZE = 512
-DELTA_TABLE = f"test-lake/{BOT_NAME}"
+DELTA_TABLE = f"housingdatalake/{BOT_NAME}"
 
 # Logging settings
 # LOG_FILE = "logs/olx.log"
--- scrapers/otodom/otodom/pipelines.py
+++ scrapers/otodom/otodom/pipelines.py
@@ -2,8 +2,78 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from dataclasses import asdict
+
+import polars as pl
+from scrapy.exporters import PythonItemExporter
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class OtodomPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class BatchDeltaExportPipeline:
+    def __init__(self, batch_size, delta_endpoint, delta_table, delta_id, delta_key):
+        self.batch_size = batch_size
+        self.delta_endpoint = delta_endpoint
+        self.delta_table = delta_table
+        self.delta_id = delta_id
+        self.delta_key = delta_key
+        self.batch_items = []
+        self.exporter = PythonItemExporter()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        batch_size = settings.getint("EXPORT_BATCH_SIZE")
+        delta_endpoint = settings.get("AWS_ENDPOINT_URL")
+        delta_table = settings.get("DELTA_TABLE")
+        delta_id = settings.get("AWS_ACCESS_KEY_ID")
+        delta_key = settings.get("AWS_SECRET_ACCESS_KEY")
+        return cls(batch_size=batch_size, delta_endpoint=delta_endpoint,
+                    delta_table=delta_table, delta_id=delta_id, delta_key=delta_key)
+
+    def open_spider(self, spider):
+        """ on spider open, create pulsar client and producer and initialize scrapy exporter. """
+        pass
+
+    def close_spider(self, spider):
+        """ on spider close, export remaining items and close pulsar producer. """
+        if self.batch_items:
+            self.export_batch()
+
+    def process_item(self, item, spider):
+        """ Process items in batches (export only when batch is full). """
+        self.batch_items.append(asdict(item))
+        if len(self.batch_items) >= self.batch_size:
+            self.export_batch()
+        return item
+
+    def export_batch(self):
+        if self.batch_items:
+            df = pl.DataFrame(self.batch_items)
+            df.write_delta(
+                target=f"s3://{self.delta_table}",
+                mode="append",
+                overwrite_schema=True,
+                storage_options={
+                    "AWS_ENDPOINT_URL": self.delta_endpoint,
+                    "AWS_ACCESS_KEY_ID": self.delta_id,
+                    "AWS_SECRET_ACCESS_KEY": self.delta_key,
+                    "AWS_REGION": "us-east-1",
+                    "AWS_ALLOW_HTTP": "true",
+                    "AWS_S3_ALLOW_UNSAFE_RENAME": "true",
+                    },
+            )
+            self.batch_items = []
+
+
+class RootImagesPipeline(ImagesPipeline):
+    """gets rid of scrapy's hardcoded '/full/' path"""
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        """This is the method used to determine file path"""
+        path = super().file_path(request, response, info)
+        return path.replace("full/", "")
